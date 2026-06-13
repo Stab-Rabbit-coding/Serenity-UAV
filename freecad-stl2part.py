ce38c3b47ab245666b548f5351a2aa2490589f58
Steve Griffing, PE(CSE), CISSP-ISSEP, CEH <steve@griffing.tech>
2026-06-13 16:16:32 -0400

    lint

--- freecad-stl2part.py
+++ freecad-stl2part.py
@@ -3,13 +3,14 @@ import FreeCAD as App
 import Mesh
 import Part
 
+
 def convert_stl_to_shape(stl_path, step_path):
     doc = App.newDocument("Headless")
 
     # Load and build geometry
     mesh_obj = Mesh.Mesh(stl_path)
     shape = Part.Shape()
-    shape.makeShapeFromMesh(mesh_obj.Topology, 0.1) # 0.1 is standard tolerance
+    shape.makeShapeFromMesh(mesh_obj.Topology, 0.1)  # 0.1 is standard tolerance
 
     feature = doc.addObject("Part::Feature", "ShapeObject")
     feature.Shape = shape
@@ -19,7 +20,6 @@ def convert_stl_to_shape(stl_path, step_path):
     Part.export([solid], step_path)
     print(f"Successfully converted {stl_path} to {step_path}")
 
-if __name__ == "__main__":
+if __name__ ==  "__main__":
     # Pull paths directly from the command line arguments
     convert_stl_to_shape(sys.argv[1], sys.argv[2])
-
