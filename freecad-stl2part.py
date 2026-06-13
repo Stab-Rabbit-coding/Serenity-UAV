487bf74c7afb3a086b5d23d721787b7193aaa349
Steve Griffing, PE(CSE), CISSP-ISSEP, CEH <steve@griffing.tech>
2026-06-13 16:56:14 -0400

    again we try

--- freecad-stl2part.py
+++ freecad-stl2part.py
@@ -21,6 +21,7 @@ def convert_stl_to_shape(stl_path, step_path):
     print(f"Successfully converted {stl_path} to {step_path}")
 
 if __name__ == "__main__":
-    
+  
+
     # Pull paths directly from the command line arguments
     convert_stl_to_shape(sys.argv[1], sys.argv[2])
